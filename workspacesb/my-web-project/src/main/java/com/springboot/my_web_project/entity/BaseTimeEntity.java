e143cf3bbec207b2213a5415b2519be13d8b8f26
김형태 <94159810+KHyungTae@users.noreply.github.com>
2026-05-19 14:32:19 +0900

    Update BaseTimeEntity.java

--- workspacesb/my-web-project/src/main/java/com/springboot/my_web_project/entity/BaseTimeEntity.java
+++ workspacesb/my-web-project/src/main/java/com/springboot/my_web_project/entity/BaseTimeEntity.java
@@ -13,15 +13,28 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.Getter;
 
 @MappedSuperclass //직접 테이블로 안만들고, 상속받은 자식클래스에 필드(컬럼)만 넘겨줌
-@EntityListeners(AuditingEntityListener.class) //데이터가 생성되거나 수정될 때 이를 감지해서 날짜를 자동으로 넣어주는 역할
+//@EntityListeners(AuditingEntityListener.class) //데이터가 생성되거나 수정될 때 이를 감지해서 날짜를 자동으로 넣어주는 역할
 @Getter
 public abstract class BaseTimeEntity {
 
-	@CreatedDate //생성 시 자동 저장
+	//@CreatedDate //생성 시 자동 저장
     @Column(name = "CREATED_DT", updatable = false) //수정 시 이 컬럼은 건드리지 않음
     private LocalDateTime createdDt;
 
-    @LastModifiedDate //수정 시 자동 갱신
-    @Column(name = "UPDATE_DT", columnDefinition = "DATE")
+    //@LastModifiedDate //수정 시 자동 갱신
+    @Column(name = "UPDATE_DT")
     private LocalDateTime updateDt;
+    
+    //가입할때 딱 1번 실행
+    @PrePersist
+    public void prePersist() {
+        this.createdDt = LocalDateTime.now();
+        this.updateDt = null; // 가입 시 명시적으로 null 유지
+    }
+    
+    //수정할때마다 실행
+    @PreUpdate
+    public void preUpdate() {
+        this.updateDt = LocalDateTime.now(); // 수정 시에만 갱신
+    }
 }
