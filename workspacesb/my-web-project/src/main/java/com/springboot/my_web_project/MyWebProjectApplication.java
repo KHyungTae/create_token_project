903657093bbf612924edb6d0ea8228dc92a40dc3
김형태 <94159810+KHyungTae@users.noreply.github.com>
2026-05-19 14:41:34 +0900

    Update MyWebProjectApplication.java

--- workspacesb/my-web-project/src/main/java/com/springboot/my_web_project/MyWebProjectApplication.java
+++ workspacesb/my-web-project/src/main/java/com/springboot/my_web_project/MyWebProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing //자동 날짜 기록 기능을 사용하게 해줌.
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MyWebProjectApplication {
 
